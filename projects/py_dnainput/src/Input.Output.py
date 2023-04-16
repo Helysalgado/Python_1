be56ccc1117edc1b028d5a5e3d53afeb048d0600
Bernardo Chombo Alvarez <bchombo@lcg.unam.mx>
2023-04-16 16:16:22 -0600

    Input.Output correction

--- projects/py_dnainput/src/Input.Output.py
+++ projects/py_dnainput/src/Input.Output.py
@@ -1,3 +1,23 @@
+## ---------------------------
+##
+## Script name: Input.Output
+##
+##
+## Author: Bernardo Chombo Álvarez
+##
+## Date Created: 2023-03-30
+##
+## Copyright (c) Bernardo, Chombo-Álvarez 2023
+## Email: bchombo@lcg.unam.mx
+##
+## ---------------------------
+##
+## Notes: This script outputs a fasta file from a DNA sequence found in 'dna.txt' file which is found in 'data/' folder inside this project.
+##
+## IMPORTANT: It first sets the work directory to the path where 'dna.txt' is found. Change it to own directory so it functions. User can download 'dna.txt' form 'data/' folder inside this project.
+##
+## ---------------------------
+
 import os
 os.chdir(r"c:\Users\chomb\Documents\GitHub\Python_1\projects\py_dnainput\data")
 
