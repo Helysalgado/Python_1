d8918bcff6ac117f8dd89229d18acd83003ffc33
Bernardo Chombo Alvarez <bchombo@lcg.unam.mx>
2023-04-18 20:28:22 -0600

    3rd modification input.output

--- projects/py_dnainput/src/Input.Output.py
+++ projects/py_dnainput/src/Input.Output.py
@@ -23,13 +23,17 @@ os.chdir(r"c:\Users\chomb\Documents\GitHub\Python_1\projects\py_dnainput\data")
 
 #Defining variables
 #Opens the file 'dna.txt'
-ip_file = open(r"dna.txt")
+print('Enter de input file:')
+f = input().rstrip("\n")
+ip_file = open(f)
 
 #Reads the file and cuts the \n silent character
 ip_read = ip_file.read().rstrip("\n")
 
 #Creating the file '.fna' and overwriting it with what it is inside the original file
-with open("dna.fna", "w") as file:
+print('Enter the new file name:')
+newname = input().rstrip("\n")
+with open(newname, "w") as file:
     file.write(ip_read)
     
 print("\nNEW FILE CREATED\n")
