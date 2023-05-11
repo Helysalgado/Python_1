9341b1a0a59bd7063f23fcb9a57a1b41e84ead1d
Bernardo Chombo Alvarez <bchombo@lcg.unam.mx>
2023-05-10 23:25:42 -0600

    full rockscissorspaper project v1.0

---
+++ projects/py_rockscissorspaper/doc/README.md
@@ -0,0 +1,29 @@
+<p float="left">
+  <img img style="float: left;"width ="200" height="100" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEji0v55N5TAUIk5h0XzQKtqmlg3kpEx11_6PZqPs4juvDGyXVfcGYIRWozxTAqYACiKkBEpKABjS9Md7t8mYycBDzNC_fXUvQVE8gMw9E0Fr9DstnrFBectnz2uZWr3r_UzxNjZ771ja7l18zeVib3K7DPiHfeTCNz7MRxsAR3FGCPSLcECZod7Tcnc/s320/Logo%20UNAM%20Morelos%20letras.png"/>             
+  <img img style="float: right;" width ="60" height="100" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiqqbbUKBPSySIHU3QZIBXVIE0Hm02hAqzlkq7e3_xU_jRRvvvzYBRXKemheQaMYmv5hgPijN-LPMDQoqRX7dPaFSTZ-fQMe9UCbyK3nKiD7Jb__tIAWgcvrGTbZvcqDB-zo2pTp7qILY8-vr2djypOrYabQTaTXEqOzrTLrUJIUazIzPt7Upw6T0ax/s320/Logo%20LCG.png"/>   
+</p>
+<br>
+<br>
+<p align="center">  <font size="3">Universidad Nacional Autónoma de México, Campus Morelos</font> <br>
+<font size="2"> Licenciatura en Ciencias Genómicas, Semestre 2023-2</font> <br>
+<font size="2"> Python, rockscissorspaper.py</font> 
+<p/>
+<div style="text-align: justify">
+
+### Bernardo Chombo Álvarez
+## Antecedentes
+La finalidad de este programa es reforzar los conocimientos aprendidos en clase que respectan a la sección de loops.
+
+## Problema
+Se necesita crear un programa que emule el juego de piedra, papel o tijera. El programa se creó de modo que el primero que llegue a 2 vencidas será el ganador del juego
+
+## Metodología
+1. El programa te da a elegir la opción que deseas jugar (piedra, papel o tijera).
+2. La computadora elige de manera aleatoria entre esas tres opciones.
+3. El programa compara las cadenas y almacena en el valor de 'lose' si perdió el usuario o en el de 'win' si este ganó.
+4. El programa termina cuando 'lose' o 'win' es igual a 2.
+
+## Resultados
+![Resultados](https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi290JKIwfSy9Pjp3lDHhXee3r0J94Z7e7k5gpUv55j4XIA5-E271qy4flQcHbgE_HQbb_HFTKQ6GRmvAOLJDprW7eEAyLrEmKNtExMoPPnNqe8p2cMXRmHi9EkQ6pqzHpi3BENMBHVNe1PxUq5_Kxcq2xzdGtPi1WGKj9027QQZ071zA_1-wOk8HQ2/s500/rockscissorspaper.png)
+
+</div>
---
+++ projects/py_rockscissorspaper/src/rockscissorspaper.py
@@ -0,0 +1,103 @@
+## ---------------------------
+##
+## Script name: rockscissorspaper.py
+##
+##
+## Author: Bernardo Chombo Álvarez
+##
+## Date Created: 2023-04-17
+##
+## Copyright (c) Bernardo, Chombo-Álvarez 2023
+## Email: bchombo@lcg.unam.mx
+##
+## ---------------------------
+##
+## Notes: This is an interactive script that allows the user to play 'rock-scissors-paper' game vs. the computer. The game is of 2 out of 3 wins
+##
+## IMPORTANT: No complementary files. 
+##
+## ---------------------------
+
+import random
+import os
+
+win = 0
+lose = 0
+i = 0
+
+print('Please enter yout option to play:\n')
+print('\ta. piedra\n\tb. papel\n\tc. tijera')
+
+#loop with two conditions: no more than 2 wins and no more than 2 losts.
+while win < 2 and lose < 2:
+    i += 1
+    options = ['piedra','papel','tijera']
+
+    print(f'\n**NOW PLAYING GAME {i}')
+
+    choice = input().rstrip("\n")
+
+    #Set the variable 'response' to a random value contained inside list
+    response = random.choice(options)
+    
+    #First checking that the input is done correctly
+    if choice not in options:
+        print('\n\t***INVALID GAME FEATURE***')
+        print('Quiting game...')
+        break
+
+    #If it is not a tie, then all options display
+    elif response != choice:
+
+        #Program evaluates according to 'response' variable. In every possible game, there are only 3 options: a tie, win or lose; so it starts with loosing option, followed by winning option
+        match response:
+            case 'piedra':
+                if choice == 'tijera':
+                    lose += 1
+                    print(f'Computer choice:\t{response}')
+                    print(f'Your choice:\t\t{choice}')
+                    print(f'FINAL RESULT:\t\tPERDISTE')
+                else:
+                    win += 1
+                    print(f'Computer choice:\t{response}')
+                    print(f'Your choice:\t\t{choice}')
+                    print(f'FINAL RESULT:\t\tGANASTE')
+            case 'tijera':
+                if choice == 'papel':
+                    lose += 1
+                    print(f'Computer choice:\t{response}')
+                    print(f'Your choice:\t\t{choice}')                
+                    print(f'FINAL RESULT:\t\tPERDISTE')
+                else:
+                    win += 1
+                    print(f'Computer choice:\t{response}')
+                    print(f'Your choice:\t\t{choice}')                
+                    print(f'FINAL RESULT:\t\tGANASTE')
+            case 'papel':
+                if choice == 'piedra':
+                    lose += 1
+                    print(f'Computer choice:\t{response}')
+                    print(f'Your choice:\t\t{choice}')                
+                    print(f'FINAL RESULT:\t\tPERDISTE')
+                else:
+                    win += 1
+                    print(f'Computer choice:\t{response}')
+                    print(f'Your choice:\t\t{choice}')                
+                    print(f'FINAL RESULT:\t\tGANASTE')                    
+    
+    #Implementing tie option
+    else:
+        print(f'Computer choice:\t{response}')
+        print(f'Your choice:\t\t{choice}')                
+        print(f'FINAL RESULT:\t\tEMPATE')
+
+#Printing all the results
+print('\n**FINAL RESULTS**')
+if win > lose:
+    print('CONGRATULATIONS! YOU WON')
+else:
+    print('SORRY FELLA, YOU LOST')
+print(f'wins ->\t\t{win}')
+print(f'losts ->\t{lose}')
+
+
