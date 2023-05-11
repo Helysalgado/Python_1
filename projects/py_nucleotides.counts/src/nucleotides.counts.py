6ac11c7b79f34788c7f5291161d1dd65505e1ef3
Bernardo Chombo Alvarez <bchombo@lcg.unam.mx>
2023-05-10 23:07:01 -0600

    nucleotides.counts updated

--- projects/py_nucleotides.counts/src/nucleotides.counts.py
+++ projects/py_nucleotides.counts/src/nucleotides.counts.py
@@ -18,12 +18,75 @@
 ##
 ## ---------------------------
 
-#INTERACTIVE
-#Using a variable set true for while loop
+import os,re
+
+#Function to obtain the directory and checking if it is empty or not 
+def location():
+    print('Enter the path where the target file is located:')
+    path = input().rstrip("\n")
+    os.chdir(path)
+    directory = os.getcwd()
+    print(f'\nYou are currently working on:\n\t{os.getcwd()}\n')
+
+    #Checking directory content
+    contents = os.listdir(directory)
+    if contents:
+        print(f'Directory content:')
+        print(*contents, sep = "\n")
+    else:
+        print('\n\tERROR! EMPTY DIRECTORY\n\tQuiting program...\n')
+        exit()
+
+#Obteining the input file
+def validating_file():
+    try:
+        print('\nEnter the input file:')
+        file = input().rstrip("\n")
+        print('\nValidating file...')
+
+        #Checking if file is not empty
+        if os.stat(file).st_size > 0:
+            print('File verified successfuly.Exists')
+        else:
+            print('\n\tERROR! EMPTY FILE\n\tQuiting program...\n')
+            exit()
+        
+        #Validating file
+        file = open(file).readlines()
+        file = concat_file(file)
+        valid_nt(file)
+
+    except OSError:
+        print('\n\tERROR! NON EXISTING FILE\n\tQuiting program...\n')
+        exit()
+    
+    return file
+
+#Function that concatenates lines inside the file
+def concat_file(file):
+    print('Reading file content...')
+    concat = ''
+    for line in file:
+        concat += line
+    concat = concat.replace("\n", "")
+    
+    return concat
+
+#Function that checks if file only contains "A","T","G","c"
+def valid_nt(file):
+    print('Verifying file content...')
+    if re.search(r"[^ATGC]",file):
+        print('\n\tERROR! INVALID FILE CONTENT\n\tQuiting program...\n')
+        exit()
+    else:
+        print('File content verified successfuly')
+
+
+#MAIN
 flag = True
 while flag:
-    print('\nInput the target sequence to analyse')
-    dna = input().rstrip("\n")
+    location()
+    dna = validating_file()
 
     #Counting de occurences
     a = dna.count("A")
